b99fc05befc3a77047c1837355636cac073a8eab
Oleg Chibikov <ochibikov@gmail.com>
2021-07-10 12:59:46 +1000

    API project skeleton

---
+++ CurrencyConverter.Api/CurrencyConverter.Api/Controllers/ConversionRatesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CurrencyConverter.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConversionRatesController : ControllerBase
+    {
+        private readonly ILogger<ConversionRatesController> _logger;
+
+        public ConversionRatesController(ILogger<ConversionRatesController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
